b162c5e715790c7b21146ae27f90cb1438c5dd59
Raymundo <rayovims@gmail.com>
2019-06-27 13:12:21 -0400

    header update

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Logo from '../../images/UPS_Logo.png';
+import './header.css';
 
 export default class Header extends React.Component {
     render () {
         return (
-            <div>Header</div>
+            <div className="row">
+                <div className='col-12 header text-center'>
+                    <img src={Logo} alt="Logo" height="100vh"/>
+                </div>
+            </div>
         )
     }
 }
