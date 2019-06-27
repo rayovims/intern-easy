7b50db51b6a94b06273ac03bd0d370053648481b
Raymundo <rayovims@gmail.com>
2019-06-27 11:38:33 -0400

    ready to build

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -2,12 +2,10 @@ const path = require("path");
 const router = require("express").Router();
 const db = require("../models/user");
 
-router.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-})
-
-router.post("/api/saveUsers", function (req, res) {
-    db.create({name: "Steve"});
+router.get("/api/saveUsers", function (req, res) {
+    db.create({name: "Raymundo", id: 1})
+    .then(dbModel => res.json(dbModel))
+    .catch(err => res.json(err));
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,3 +1,33 @@
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const path = require('path');
+// const mongoose = require("mongoose");
+// const controllers = require("./controllers/controllers");
+
+// const app = express();
+// const port = process.env.PORT || 5000;
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, "client", "build")))
+// app.use(controllers);
+
+// mongoose.Promise = global.Promise;
+
+// mongoose.connect("mongodb://localhost:27017/internEasy",
+//     { useNewUrlParser: true }
+// );
+
+// if (process.env.NODE_ENV === 'production') {
+//   // Serve any static files
+//   app.use(express.static(path.join(__dirname, 'client/build')));
+//   // Handle React routing, return all requests to React app
+//   app.get('*', function(req, res) {
+//     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+//   });
+// }
+// app.listen(port, () => console.log(`Listening on port ${port}`));
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
@@ -5,11 +35,9 @@ const mongoose = require("mongoose");
 const controllers = require("./controllers/controllers");
 
 const app = express();
-const port = process.env.PORT || 3001;
-
+const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "client", "build")))
 app.use(controllers);
 
 mongoose.Promise = global.Promise;
@@ -17,6 +45,7 @@ mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost:27017/internEasy",
     { useNewUrlParser: true }
 );
+
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
