219d9a078a786ecd874ff9d6ad01f26f525a52d7
Raymundo <rayovims@gmail.com>
2019-06-27 15:42:17 -0400

    modals

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -2,8 +2,9 @@ const path = require("path");
 const router = require("express").Router();
 const db = require("../models/user");
 
-router.get("/api/saveUsers", function (req, res) {
-    db.create({name: "Raymundo", id: 1})
+router.post("/api/create/candidate", function (req, res) {
+    console.log(req.body);
+    db.create(req.body)
     .then(dbModel => res.json(dbModel))
     .catch(err => res.json(err));
 })
