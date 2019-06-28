b51642d6dd41ade2418bfe863d6463a6d56c7a15
Courtney <Rhodesc1207@gmail.com>
2019-06-28 08:28:21 -0400

    HRPORTAL
    BOOM

--- client/src/components/HRPortal/HRPortal.js
+++ client/src/components/HRPortal/HRPortal.js
@@ -1,9 +1,45 @@
 import React from 'react';
-
+import './hrportal.css';
 export default class HRPortal extends React.Component {
     render () {
         return (
-            <div>HRPortal</div>
-        )
-    }
+          <div> <center>
+     <h2>Hiring Manager Top 3 Applicants</h2>
+     <p>Move the mouse over a button to see the Manager's top applicants.</p>
+     <div className="dropdown">
+       <button className="dropbtn">Trisha Jacobson</button>
+       <div className="dropdown-content">
+         <a href="#">Corrine Henson</a>
+         <a href="#">Josie Reed</a>
+         <a href="#">Roger Key</a>
+       </div>
+     </div>
+     <div className="dropdown">
+       <button className="dropbtn">Derek Robertson</button>
+       <div className="dropdown-content">
+         <a href="#">Alfred Kim</a>
+         <a href="#">Roberta Mckee</a>
+         <a href="#">Clifton Hudson</a>
+       </div>
+     </div>
+     <div className="dropdown">
+       <button className="dropbtn">Herman Floyd</button>
+       <div className="dropdown-content">
+         <a href="#">Sonia Gonzales</a>
+         <a href="#">Sheri Dennis</a>
+         <a href="#">Serena Serrano</a>
+       </div>
+     </div>
+     <div className="dropdown">
+       <button className="dropbtn">Percy Watkins</button>
+       <div className="dropdown-content">
+         <a href="#">Judy Garner</a>
+         <a href="#">Thaddeus Lester</a>
+         <a href="#">Leandro Cervantes</a>
+       </div>
+     </div>
+     </center>
+   </div>
+ );
 }
+};
