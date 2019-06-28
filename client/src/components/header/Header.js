a79a8d148f4b105986ed061e9fa57c8213175086
Courtney <Rhodesc1207@gmail.com>
2019-06-27 21:30:49 -0400

    login page

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -2,14 +2,22 @@ import React from 'react';
 import Logo from '../../images/UPS_Logo.png';
 import './header.css';
 
+const jumbotronStyle = {
+
+};
+
 export default class Header extends React.Component {
     render () {
         return (
-            <div className="row">
-                <div className='col-12 header text-center'>
-                    <img src={Logo} alt="Logo" height="100vh"/>
+            //<div className="row">
+          // <div className="container-fluid">
+            <div className="jumbotron text-center">
+                    <img className ="upsLogo" src={Logo} alt="Logo" height="100vh"/>
+                    <h5 className = "h5fix"> <b>Intern Requisition Portal</b> </h5>
                 </div>
-            </div>
+            //</div>
+//            </div>
+      //    </div>
         )
     }
 }
--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,9 +1,24 @@
 import React from 'react';
-
+import './login.css';
 export default class Login extends React.Component {
     render () {
         return (
-            <div>Login Page</div>
+          <div>
+    <div className="container">
+      <form method="post" action="/Login">
+      <label htmlFor="uname"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="uname" required />
+      <label htmlFor="psw"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" required />
+      <center>
+      <button className = "loginBn" type="submit">Login</button>
+      </center>
+      </form>
+    </div>
+
+
+
+          </div>
         )
     }
 }
